a5706cac205375a1c1820d1eff7df91d1b1a7e84
max <125398440+themax6600@users.noreply.github.com>
2025-03-30 13:35:07 -0300

    30/03/25

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,98 +1,92 @@
-    var header = document.getElementById('header');
-    var navigationHeader = document.getElementById('navigation_header');
-    var showSidebar = false;
+var header = document.getElementById('header');
+var navigationHeader = document.getElementById('navigation_header');
+var showSidebar = false;
 
-    function toggleSidebar()
-    {
-        showSidebar = !showSidebar;
-        if(showSidebar)
-        {
-            navigationHeader.style.marginLeft = '-10vw';
-            navigationHeader.style.animationName = 'showSidebar';
-        }
-        else
-        {
-            navigationHeader.style.marginLeft = '-100vw';
-            navigationHeader.style.animationName = '';
-        }
+function toggleSidebar() {
+    showSidebar = !showSidebar;
+    if (showSidebar) {
+        navigationHeader.style.marginLeft = '-10vw';
+        navigationHeader.style.animationName = 'showSidebar';
     }
-
-    function closeSidebar()
-    {
-        if(showSidebar)
-        {
-            showSidebar = true;
-            toggleSidebar();
-        }
+    else {
+        navigationHeader.style.marginLeft = '-100vw';
+        navigationHeader.style.animationName = '';
     }
+}
 
-    window.addEventListener('resize', function(event) {
-        if(window.innerWidth > 820 && showSidebar) 
-        {  
-            showSidebar = true;
-            toggleSidebar();
-        }
-    });
+function closeSidebar() {
+    if (showSidebar) {
+        showSidebar = true;
+        toggleSidebar();
+    }
+}
+
+window.addEventListener('resize', function (event) {
+    if (window.innerWidth > 820 && showSidebar) {
+        showSidebar = true;
+        toggleSidebar();
+    }
+});
 
 
 
 
 let slides = document.querySelectorAll('.slideshow');
-        let dots = document.querySelectorAll('.dot');
-        let slideIndex = 1;
+let dots = document.querySelectorAll('.dot');
+let slideIndex = 1;
 
-        const showSlides = (n) => {
-            let i;
+const showSlides = (n) => {
+    let i;
 
-            if (n > slides.length) {
-                slideIndex = 1;
-            }
-            if (n < 1) {
-                slideIndex = slides.length;
-            }
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
 
-            for (i = 0; i < slides.length; i++) {
-                slides[i].style.display = "none";
-            }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
 
-            for (i = 0; i < slides.length; i++) {
-                dots[i].setAttribute('class', 'dot');
-            }
+    for (i = 0; i < slides.length; i++) {
+        dots[i].setAttribute('class', 'dot');
+    }
 
-            slides[slideIndex - 1].style.display = 'block';
-            dots[slideIndex - 1].setAttribute('class', 'dot ativo');
-            clearTimeout(timeoutID);
-            timeoutID = setTimeout(autoSlides, 4000);
-        };
+    slides[slideIndex - 1].style.display = 'block';
+    dots[slideIndex - 1].setAttribute('class', 'dot ativo');
+    clearTimeout(timeoutID);
+    timeoutID = setTimeout(autoSlides, 4000);
+};
 
-        const plusSlides = (n) => {
-            showSlides(slideIndex += n);
-        };
+const plusSlides = (n) => {
+    showSlides(slideIndex += n);
+};
 
-        const currentSlide = (n) => {
-            showSlides(slideIndex = n);
-        };
+const currentSlide = (n) => {
+    showSlides(slideIndex = n);
+};
 
-        function autoSlides() {
-            let i;
+function autoSlides() {
+    let i;
 
-            for (i = 0; i < slides.length; i++) {
-                slides[i].style.display = "none";
-            }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
 
-            slideIndex++;
-            if (slideIndex > slides.length) {
-                slideIndex = 1;
-            }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
 
-            for (i = 0; i < slides.length; i++) {
-                dots[i].setAttribute('class', 'dot');
-            }
+    for (i = 0; i < slides.length; i++) {
+        dots[i].setAttribute('class', 'dot');
+    }
 
-            slides[slideIndex - 1].style.display = "block";
-            dots[slideIndex - 1].setAttribute('class', 'dot ativo');
-            timeoutID = setTimeout(autoSlides, 4000);
-        }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].setAttribute('class', 'dot ativo');
+    timeoutID = setTimeout(autoSlides, 4000);
+}
 
-        autoSlides();
+autoSlides();
 
